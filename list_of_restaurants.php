49201103dbdafe7a0f831a5125e88f9446c7c8e1
yitwah <75240613+yitwah@users.noreply.github.com>
2020-12-08 09:24:06 +0800

    Create list_of_restaurants.php

---
+++ list_of_restaurants.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+     <title>List of KL Restaurants</title>
+  </head>
+ <body>
+     <center>
+       <h1> List of KL Restaurants< </h1>
+       <table border="1">
+           <tr>
+             <th> Restaurant ID </th>
+             <th> Restaurant Name </th>
+             <th> Address </th>
+             <th> Phone </th>
+           </tr>
+           <tr>
+             <td> 0 </td>
+             <td> Subway Restaurant </td>
+             <td> Menara Standard Chartered, TPM </td>
+             <td> 03-22441234 </td>
+           </tr>
+       </table>
+     </center>
+ </body>
+</html>
